5a5025fa572939ab451a76b9367c99ecd63ec277
Eric Clute <eclute@psrc.org>
2023-03-10 14:33:49 -0800

    Update reconcile_IRHD_&_new_data.R

--- reconcile_IRHD_&_new_data.R
+++ reconcile_IRHD_&_new_data.R
@@ -10,7 +10,7 @@
 
 library(tidyverse)
 library(readxl)
-library(janitor)
+#library(janitor)
 
 
 ## 1) load data ---------------------------------------------------------------------
@@ -25,12 +25,7 @@ WSHFC22raw <- read_csv("J:/Projects/IncomeRestrictedHsgDB/2022_update/WSHFC/Clea
 # KC22raw <- read_csv("J:/Projects/IncomeRestrictedHsgDB/2022_update/Review Files - Received/")
 
 
-## 2) clean up fields in IRHD, limit to 3 counties, add new fields --------------------------------------------------------------------
-
-# Set field types! The XY coordinates are currently missing data beyond the decimal points
-
-
-
+## 2) clean up fields in IRHD, limit to 3 counties, add/remove fields --------------------------------------------------------------------
 
 # Create three new HOME fields
 IRHD22raw <- IRHD22raw %>%
@@ -43,6 +38,10 @@ IRHD22raw <- IRHD22raw[, c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,
                            21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,
                            41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,
                            61,62,63,64,65,66,67,68,69,78,79,80,70,71,72,73,74,75,76,77)]
+
+# Remove summary AMI fields (we can do this all via a script from now on)
+
+IRHD22raw <- IRHD22raw[, -c(40,41,42,43,44)]
 
 # Clean up various fields for matching with WSHFC
 IRHD22raw$Manager[IRHD22raw$Manager == 'HASCO'] <- 'Snohomish County Housing Authority'
@@ -190,8 +189,4 @@ long_WSHFC <- long_WSHFC[c(2,4,5)]
 long_compare <- long_IRHD %>%
   inner_join(long_WSHFC, by=c('PropertyID', 'variable_class')) %>%
   filter(variable_value.x != variable_value.y)
-
-
-
-
 
