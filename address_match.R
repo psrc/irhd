5abe7857a686833522cb0aa92c1db8a56b9bcfdc
Chris Peak <CPeak@psrc.org>
2023-05-23 08:14:07 -0700

    add address_match.R (add_cleaned_addresses() function :

---
+++ address_match.R
@@ -0,0 +1,47 @@
+library(postmastr)
+library(tidyverse)
+
+
+add_cleaned_addresses <- function(in.df) {
+  # returns a data frame with a new column "cleaned.address" that is the formatted version of input column "fulladdress"
+  # required parameter in.df must be a data frame containing a column "fulladdress"
+  # that contains full address info (house-number street, city-name, WA, ZIP Code)
+  in.df <- df
+
+  cities <- pm_dictionary(type='city', filter="WA", case=c("title", "upper"), locale="us")
+  in.df <- in.df %>%
+    mutate(fulladdress = str_replace_all(fulladdress, "N.W.", "NW"))
+
+  df_ident <- pm_identify(in.df, var='fulladdress', locale="us")
+  df_min <- pm_prep(df_ident, var='fulladdress', type='street')
+
+  my_dirs <- pm_append("directional",
+           input=c("N.W.", "N.E.", "S.W.", "S.E.", "N.W", "N.E", "S.W", "S.E"),
+           output=c("NW", "NE", "SW", "SE", "NW", "NE", "SW", "SE"),
+           locale="us")
+  dirs <- pm_dictionary("directional", append=my_dirs)
+
+  df_min <- pm_postal_parse(df_min)
+  df_min <- pm_state_parse(df_min)
+  df_min <- pm_city_parse(df_min, dictionary=cities)
+  df_min <- pm_house_parse(df_min)
+  df_min <- pm_houseRange_parse(df_min)
+  df_min <- pm_houseFrac_parse(df_min)
+
+  df_min <- pm_streetDir_parse(df_min, dictionary=dirs)
+  df_min <- pm_street_parse(df_min)
+  df_parsed <- pm_replace(df_min, source=df_ident)
+  df_parsed <- pm_rebuild(df_parsed,
+                          output="full",
+                          include_commas=TRUE,
+                          keep_parsed="no",
+                          keep_ids=TRUE) %>%
+    select(c('pm.id', 'pm.address'))
+
+  out.df <- df_ident %>%
+    left_join(df_parsed, by='pm.id') %>%
+    select(-c('pm.id', 'pm.uid', 'pm.type')) %>%
+    rename('cleaned.address' = 'pm.address')
+
+  return(out.df)
+}
