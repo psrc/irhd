d10639403beef949ef9e3d06ba7f51fdb6756f89
Eric Clute <eclute@psrc.org>
2023-09-22 15:28:14 -0700

    updates to scripts

---
+++ create_summary_tables.sql
@@ -0,0 +1,12 @@
+-- Summarizing IRHD Data
+--- As outlined in http://aws-linux/mediawiki/index.php/Income-Restricted_Household_Data
+
+DECLARE @vintage AS VARCHAR(100)='2021'
+
+-- Summarize by County
+select *
+from irhd.summarize_by_county(@vintage)
+
+--Summarize by Jurisdiction
+select *
+from irhd.summarize_by_jurisdiction(@vintage)
