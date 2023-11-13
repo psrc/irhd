ebd0e6d19dff93e2314bc7d471f85d5eaea946f4
Chris Peak <CPeak@psrc.org>
2023-11-13 15:27:09 -0800

    change path to address_match.R to a relative path

--- clean_2022_WSHFC_data.R
+++ clean_2022_WSHFC_data.R
@@ -16,7 +16,7 @@ WSHFC_path <- "J:/Projects/IncomeRestrictedHsgDB/2022 vintage/Data/WSHFC/"
 WSHFC_raw <- read_xlsx(paste0(WSHFC_path, "PSRC report for 2022.xlsx"))
 WSHFC_clean_file <- "WSHFC_2022_cleaned.csv"
 vintage_year = "2022"
-address_scrpt <- "C:/Users/eclute/OneDrive - Puget Sound Regional Council/Documents/GitHub/irhd/address_match.R"
+address_scrpt <- "./address_match.R"
 
 remotes::install_github("slu-openGIS/postmastr")
 source(address_scrpt)
@@ -163,7 +163,7 @@ WSHFC_cleaned %>%
   arrange(`Project Name`, `Site Name`, Address) %>%
   view()
 
-# ------- DATA FILTER #5 ------- Small edits/checks 
+# ------- DATA FILTER #5 ------- Small edits/checks
 
 #Filter by InServiceDate - select only records in this vintage year or earlier
 WSHFC_cleaned <- WSHFC_cleaned %>%
@@ -246,9 +246,9 @@ WSHFC_cleaned$reported_address[WSHFC_cleaned$reported_address == '9225 Bayshore 
 WSHFC_cleaned$reported_address[WSHFC_cleaned$reported_address == '9239 Bayshore Dr NW'] <- '9239 Bay Shore Dr NW'
 
 # Clean address field for matching
-# WSHFC_cleaned$full_address <- str_c(WSHFC_cleaned$reported_address,', ',WSHFC_cleaned$city,', WA, ',WSHFC_cleaned$zip)
-# WSHFC_cleaned_test <- add_cleaned_addresses(WSHFC_cleaned)
-# 
+WSHFC_cleaned$full_address <- str_c(WSHFC_cleaned$reported_address,', ',WSHFC_cleaned$city,', WA, ',WSHFC_cleaned$zip)
+WSHFC_cleaned_test <- add_cleaned_addresses(WSHFC_cleaned)
+#
 # str(WSHFC_cleaned)
 
 ## 5) save file --------------------------------------------------------------------
